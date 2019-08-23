0b4b521c1881e394402c07e68fe55f74d4e9b1e6
MYiLA <polus180@ya.ru>
2019-08-23 21:03:52 +0300

    PP mobile index

--- source/js/script.js
+++ source/js/script.js
@@ -1,37 +1,37 @@
-var nav = document.querySelector('.nav');
-var iframe = document.querySelector('.contacts__map-iframe');
-var navMenuBtn = document.querySelector('.nav__menu-btn');
-var labelName = document.querySelector('[name=name]');
-var labelWeight = document.querySelector('[name=weight]');
-var labelEmail = document.querySelector('[name=email]');
-var labelTel = document.querySelector('[name=tel]');
-var form = document.querySelector('.form__form');
+// var nav = document.querySelector('.nav');
+// var iframe = document.querySelector('.contacts__map-iframe');
+// var navMenuBtn = document.querySelector('.nav__menu-btn');
+// var labelName = document.querySelector('[name=name]');
+// var labelWeight = document.querySelector('[name=weight]');
+// var labelEmail = document.querySelector('[name=email]');
+// var labelTel = document.querySelector('[name=tel]');
+// var form = document.querySelector('.form__form');
 
-nav.classList.remove('nav--nojs'); 
-iframe.classList.remove('contacts__map-iframe--nojs');
+// nav.classList.remove('nav--nojs'); 
+// iframe.classList.remove('contacts__map-iframe--nojs');
 
-navMenuBtn.addEventListener('click', function () {
-  if (nav.classList.contains('nav--closed')) {
-    nav.classList.remove('nav--closed');
-    nav.classList.add('nav--opened');
-  } else {
-    nav.classList.add('nav--closed');
-    nav.classList.remove('nav--opened');
-  }
-});
+// navMenuBtn.addEventListener('click', function () {
+//   if (nav.classList.contains('nav--closed')) {
+//     nav.classList.remove('nav--closed');
+//     nav.classList.add('nav--opened');
+//   } else {
+//     nav.classList.add('nav--closed');
+//     nav.classList.remove('nav--opened');
+//   }
+// });
 
-form.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  if (!labelName.value) {
-    labelName.classList.add("form__input--invalid");
-  }
-  if (!labelWeight.value) {
-    labelWeight.classList.add("form__input--invalid");
-  }
-  if (!labelEmail.value) {
-    labelEmail.classList.add("form__input--invalid");
-  }
-  if (!labelTel.value) {
-    labelTel.classList.add("form__input--invalid");
-  }
-});
+// form.addEventListener("submit", function (evt) {
+//   evt.preventDefault();
+//   if (!labelName.value) {
+//     labelName.classList.add("form__input--invalid");
+//   }
+//   if (!labelWeight.value) {
+//     labelWeight.classList.add("form__input--invalid");
+//   }
+//   if (!labelEmail.value) {
+//     labelEmail.classList.add("form__input--invalid");
+//   }
+//   if (!labelTel.value) {
+//     labelTel.classList.add("form__input--invalid");
+//   }
+// });
