f1395730957923a056f1895a4f020ca21c5224f9
MYiLA <polus180@ya.ru>
2019-08-23 17:06:47 +0300

    фиксы по js и форме

--- source/js/script.js
+++ source/js/script.js
@@ -3,32 +3,10 @@ var navMenuBtn = document.querySelector('.nav__menu-btn');
 var labelName = document.querySelector('[name=name]');
 var labelWeight = document.querySelector('[name=weight]');
 var labelEmail = document.querySelector('[name=email]');
-var labelTel = document.querySelector('[name=Tel]');
+var labelTel = document.querySelector('[name=tel]');
 var form = document.querySelector('.form__form');
 
 nav.classList.remove('nav--nojs');
-
-form.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  if(!labelName.value) {
-    evt.preventDefault();
-labelName.classList.remove("input-invalide");
-   labelName.offsetWidth = labelName.offsetWidth;
-    labelName.classList.add("form__input--invalid");
-  }
-  if(!labelWeight.value) {
-    evt.preventDefault();
-    labelWeight.classList.add("form__input--invalid");
-  }
-  if(!labelEmail.value) {
-    evt.preventDefault();
-    labelEmail.classList.add("form__input--invalid");
-  }
-  if(!labelTel.value) {
-    evt.preventDefault();
-    labelTel.classList.add("form__input--invalid");
-  }
-});
 
 navMenuBtn.addEventListener('click', function () {
   if (nav.classList.contains('nav--closed')) {
@@ -37,5 +15,21 @@ navMenuBtn.addEventListener('click', function () {
   } else {
     nav.classList.add('nav--closed');
     nav.classList.remove('nav--opened');
+  }
+});
+
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  if (!labelName.value) {
+    labelName.classList.add("form__input--invalid");
+  }
+  if (!labelWeight.value) {
+    labelWeight.classList.add("form__input--invalid");
+  }
+  if (!labelEmail.value) {
+    labelEmail.classList.add("form__input--invalid");
+  }
+  if (!labelTel.value) {
+    labelTel.classList.add("form__input--invalid");
   }
 });
